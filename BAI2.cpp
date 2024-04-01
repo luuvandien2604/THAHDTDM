0564f416fc9f2b64d6de19c45a11212d24eff83c
luuvandien2604 <120245027+luuvandien2604@users.noreply.github.com>
2024-04-01 08:46:23 +0700

    Add files via upload

---
+++ BAI2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib> 
+
+bool LaSoChinhPhuong(int n) {
+	int sqrtN = sqrt(n);
+	return sqrtN * sqrtN == n;
+}
+
+void DemVaInSoChinhPhuong(int n) {
+	int count = 0;
+	for (int i = 1; i * i < n; i++) {
+		std::cout << i * i << " ";
+		count++;
+	}
+	std::cout << "\nCó " << count << " số chính phương nhỏ hơn " << n << std::endl;
+}
+
+int main() {
+	std::cout << "Nhập số nguyên dương n: ";
+	int n;
+	std::cin >> n;
+	DemVaInSoChinhPhuong(n);
+	system("pause"); 
+	return 0;
+}
